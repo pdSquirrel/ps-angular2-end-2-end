bbc15609fd490c9f4a6022bb9aa3b8b0c9739857
Roger Norqvist <progdev.squirrel@gmail.com>
2017-07-17 13:43:06 +0200

    initial commit

---
+++ src/app/shared/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navi-bar',
+  template: `
+    <div class="top-bar">
+      <div class="top-bar-title">Gigabyte Gaming Company</div>
+      <div>
+        <ul class="menu">
+          <li class="nav-menu">
+            <a href="#">Menu Item 1</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./navbar.component.css']
+})
+
+export class NavComponent {}
