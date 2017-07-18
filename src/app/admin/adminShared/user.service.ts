b981010a8001e28de9343113d3fe79f6a1df3878
Roger Norqvist <progdev.squirrel@gmail.com>
2017-07-18 10:27:46 +0200

    added firebase

--- src/app/admin/adminShared/user.service.ts
+++ src/app/admin/adminShared/user.service.ts
@@ -5,12 +5,24 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot
 } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class UserService implements CanActivate {
   userLoggedIn: boolean = false;
 
-  constructor( private router: Router) {}
+  constructor( private router: Router) {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCimW6L6aCfEyFYGOtY8DEBngXvNRXbbNA",
+      authDomain: "gigabytegames-df99c.firebaseapp.com",
+      databaseURL: "https://gigabytegames-df99c.firebaseio.com",
+      projectId: "gigabytegames-df99c",
+      storageBucket: "gigabytegames-df99c.appspot.com",
+      messagingSenderId: "840338801634"
+    };
+    firebase.initializeApp(config);
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     let url: string = state.url;
